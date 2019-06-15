c466568095e31a45f8bbb744fa4b34f388169d01
Zoltan Puchovsky <zoltan.puchovsky@gmail.com>
2019-06-15 16:05:00 +0200

    Saving weights view controller done

--- 531Zorzo/SetWeightsViewController.swift
+++ 531Zorzo/SetWeightsViewController.swift
@@ -10,35 +10,70 @@ import UIKit
 
 class SetWeightsViewController: UIViewController {
     
-    
+    // main text fields
     @IBOutlet weak var squatOneRepMaxInput: UITextField!
     @IBOutlet weak var deadliftOneRepMaxInput: UITextField!
     @IBOutlet weak var benchpressOneRepMaxInput: UITextField!
     @IBOutlet weak var overheadpressOneRepMaxInput: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
     
     @IBAction func saveWeights(_ sender: Any) {
+        // saving the input
         let squatOneRepMax = Double(squatOneRepMaxInput.text!)
         let deadliftOneRepMax = Double(deadliftOneRepMaxInput.text!)
         let benchpressOneRepMax = Double(benchpressOneRepMaxInput.text!)
         let overheadpressOneRepMax = Double(overheadpressOneRepMaxInput.text!)
         
-        UserDefaults.standard.set(squatOneRepMax, forKey: "SquatOneRepMaxSaved")
-        UserDefaults.standard.set(deadliftOneRepMax, forKey: "DeadliftOneRepMaxSaved")
-        UserDefaults.standard.set(benchpressOneRepMax, forKey: "BenchpressOneRepMaxSaved")
-        UserDefaults.standard.set(overheadpressOneRepMax, forKey: "OverheadpressOneRepMaxSaved")
-    
+        UserDefaults.standard.set(squatOneRepMaxInput.text, forKey: "SquatOneRepMaxSaved")
+        UserDefaults.standard.set(deadliftOneRepMaxInput.text, forKey: "DeadliftOneRepMaxSaved")
+        UserDefaults.standard.set(benchpressOneRepMaxInput.text, forKey: "BenchpressOneRepMaxSaved")
+        UserDefaults.standard.set(overheadpressOneRepMaxInput.text, forKey: "OverheadpressOneRepMaxSaved")
         
     }
     
     
-    @IBAction func resetWeights(_ sender: Any) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // displaying the saved data
+        
+        print(UserDefaults.standard.dictionaryRepresentation())
+        
+        let savedSquatOneRepMax = UserDefaults.standard.object(forKey: "SquatOneRepMaxSaved")
+        if let squatOneRepMaxDisplayed = savedSquatOneRepMax as? String {
+            squatOneRepMaxInput.text = squatOneRepMaxDisplayed
+        }
+        
+        
+        
+        let savedDeadliftOneRepMax = UserDefaults.standard.object(forKey: "DeadliftOneRepMaxSaved")
+        if let deadliftOneRepMaxDisplayed = savedDeadliftOneRepMax as? String {
+            deadliftOneRepMaxInput.text = deadliftOneRepMaxDisplayed
+        }
+        
+        
+        let savedBenchpressOneRepMax = UserDefaults.standard.object(forKey: "BenchpressOneRepMaxSaved")
+        if let benchpressOneRepMaxDisplayed = savedBenchpressOneRepMax as? String {
+            benchpressOneRepMaxInput.text = benchpressOneRepMaxDisplayed
+        }
+        
+        
+        let savedOverheadpressOneRepMax = UserDefaults.standard.object(forKey: "OverheadpressOneRepMaxSaved")
+        if let overheadpressOneRepMaxDisplayed = savedOverheadpressOneRepMax as? String {
+            overheadpressOneRepMaxInput.text = overheadpressOneRepMaxDisplayed
+        }
+        
+        
+        
+        
+        
+        
+        
+        
     }
+    
+    
     
 
 }
